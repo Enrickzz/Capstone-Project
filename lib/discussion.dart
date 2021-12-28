6f6d87421f5abefc852a833452aa035667a36b1d
giantiyan <61373410+giantiyan@users.noreply.github.com>
2021-12-29 04:57:12 +0800

    discussion

---
+++ lib/discussion.dart
@@ -0,0 +1,306 @@
+import 'package:my_app/services/auth.dart';
+import 'package:my_app/ui_view/BMI_chart.dart';
+import 'package:my_app/my_diary/area_list_view.dart';
+import 'package:my_app/ui_view/calorie_intake.dart';
+import 'package:my_app/ui_view/diet_view.dart';
+import 'package:my_app/ui_view/glucose_levels_chart.dart';
+import 'package:my_app/ui_view/grid_images.dart';
+import 'package:my_app/ui_view/heartrate.dart';
+import 'package:my_app/ui_view/running_view.dart';
+import 'package:my_app/ui_view/title_view.dart';
+import 'package:my_app/ui_view/workout_view.dart';
+import 'package:my_app/ui_view/bp_chart.dart';
+import 'package:my_app/models/nutritionixApi.dart';
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+import '../fitness_app_theme.dart';
+
+class discussion extends StatefulWidget {
+  @override
+  _discussionState createState() => _discussionState();
+}
+
+final _formKey = GlobalKey<FormState>();
+List<Common> result = [];
+List<double> calories = [];
+class _discussionState extends State<discussion>
+    with TickerProviderStateMixin {
+
+  String search="";
+  List<Widget> listViews = <Widget>[];
+  final ScrollController scrollController = ScrollController();
+
+  final AuthService _auth = AuthService();
+
+  double topBarOpacity = 0.0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 50));
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Future.delayed(const Duration(milliseconds: 5000), () {
+    //   setState(() {
+    //     print("FULL SET STATE");
+    //   });
+    // });
+    return Container(
+      color: FitnessAppTheme.background,
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+              color: Colors.black
+          ),
+          title: const Text('Discussion', style: TextStyle(
+              color: Colors.black
+          )),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(56),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Form(
+                    autovalidateMode: AutovalidateMode.onUserInteraction, key: _formKey,
+                    child: Row (
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.search),
+                                hintText: 'Search a topic',
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                    borderSide: BorderSide(
+                                      width: 0,
+                                      style: BorderStyle.none,
+                                    )
+                                ),
+                                filled: true,
+                                errorStyle: TextStyle(fontSize: 15),
+                                contentPadding: const EdgeInsets.symmetric(vertical: 12.0),
+                              ),
+                              onChanged: (val) {
+                                setState(() => search = val);
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          ElevatedButton(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
+                              child: Text('Search', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                              ),
+                            ),
+                            onPressed: () async{
+
+                            },
+                          ),
+                        ]
+                    )),
+              )
+          ),
+        ),
+        body:  Scrollbar(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(24, 28, 24, 100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                      child: Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:<Widget>[
+                            Expanded(
+                              child: Text( "Posts",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color:Color(0xFF4A6572),
+                                  )
+                              ),
+                            ),
+                            InkWell(
+                                highlightColor: Colors.transparent,
+                                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                onTap: () {},
+                                // child: Padding(
+                                // padding: const EdgeInsets.only(left: 8),
+                                child: Row(
+                                  children: <Widget>[
+                                    // Text( "Edit",
+                                    //     style: TextStyle(
+                                    //       fontSize: 16,
+                                    //       fontWeight: FontWeight.normal,
+                                    //       color:Color(0xFF2633C5),
+                                    //     )
+                                    // ),
+                                    // SizedBox(
+                                    //   height: 38,
+                                    //   width: 26,
+                                    //   // child: Icon(
+                                    //   //   Icons.arrow_forward,
+                                    //   //   color: FitnessAppTheme.darkText,
+                                    //   //   size: 18,
+                                    //   // ),
+                                    // ),
+                                  ],
+                                )
+                              // )
+                            )
+                          ]
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                          )
+                      );
+                    },
+                    child: Container(
+                      height: 180,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [BoxShadow(
+                              color: Colors.black26.withOpacity(0.05),
+                              offset: Offset(0.0,6.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 0.10
+                          )]
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              height: 70,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        backgroundImage: AssetImage('assets/images/heart_icon.png'),
+                                        radius: 22,
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 8.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            Container(
+                                              width: MediaQuery.of(context).size.width * 0.65,
+                                              child: Text(
+                                                "Painful Penis. What to do?",
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(height: 2.0),
+                                            Row(
+                                              children: <Widget>[
+                                                Text(
+                                                  "Johnny Sins",
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.grey.withOpacity(0.6)
+                                                  ),
+                                                ),
+                                                SizedBox(width: 15),
+                                                Text(
+                                                  "12/29/2021 11:10",
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.grey.withOpacity(0.6)
+                                                  ),
+                                                )
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 50,
+                              child: Center(
+                                child: Text(
+                                  "Lorem ipsum bla bla bla bla tite masakit aray ko!!!! asdasd asdasd asd asd asd asd asd asd asd asd asdasdad asd a..".substring(0,95) + "...",
+                                  style: TextStyle(
+                                    color: Colors.grey.withOpacity(0.8),
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.reply,
+                                      color: Colors.grey.withOpacity(0.6),
+                                      size: 16,
+                                    ),
+                                    SizedBox(width: 4.0),
+                                    Text(
+                                      "5 replies",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey.withOpacity(0.6)
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+
+    );
+  }
+
+}
