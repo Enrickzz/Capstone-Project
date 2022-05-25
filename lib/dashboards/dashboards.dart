e037c630441fdd629a80a7f4d5a942bae9d259a3
Enrickzz <enrico_cuison@dlsu.edu.ph>
2022-05-25 23:39:19 +0800

    Fitbit in dashboard

--- lib/dashboards/dashboards.dart
+++ lib/dashboards/dashboards.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:my_app/models/FitBitToken.dart';
 import 'package:my_app/models/users.dart';
 import 'package:my_app/ui_view/BloodGlucose_TimeChart.dart';
 import 'package:my_app/ui_view/HeartRate_TimeChart.dart';
@@ -36,6 +38,10 @@ import 'package:my_app/ui_view/workout_view.dart';
 import 'package:my_app/ui_view/blood_pressure/bp_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:http/http.dart' as http;
+import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../fitness_app_theme.dart';
 import '../main.dart';
@@ -52,6 +58,7 @@ class Dashboards extends StatefulWidget {
 class _DashboardsState extends State<Dashboards>
     with TickerProviderStateMixin {
   Animation<double> topBarAnimation;
+  String fitbitToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzg0VzQiLCJzdWIiOiI4VFFGUEQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNjQyNzg0MzgxLCJpYXQiOjE2NDI3NTU1ODF9.d3JfpNowesILgLa306QAyOJbAcPbbVZ9Aj9U-pPdCWs";
 
   List<Widget> listViews = <Widget>[];
   final ScrollController scrollController = ScrollController();
@@ -62,7 +69,18 @@ class _DashboardsState extends State<Dashboards>
   double topBarOpacity = 0.0;
   List<RecomAndNotif> notifsList = new List<RecomAndNotif>();
   List<RecomAndNotif> recommList = new List<RecomAndNotif>();
-
+  final authorizationEndpoint =
+  Uri.parse("https://www.fitbit.com/oauth2/authorize");
+  final tokenEndpoint = Uri.parse("https://api.fitbit.com/oauth2/token");
+  final identifier = "2384W4";
+  final secret = "8fa2d37b0bdf0b766d6a14f9ce64638c";
+  final redirectUrl = Uri.parse("localhost://callback");
+  final credentialsFile = new File("encrypt/credentials.json");
+  final _scopes = ['weight', 'location','settings','profile','nutrition', 'activity','sleep','heartrate','social'];
+  oauth2.AuthorizationCodeGrant grant;
+  oauth2.Client _client;
+  Uri _uri;
+  bool tokenTrue=true;
   @override
   void initState() {
     getRecomm();
@@ -71,8 +89,67 @@ class _DashboardsState extends State<Dashboards>
         CurvedAnimation(
             parent: widget.animationController,
             curve: Interval(0, 0.5, curve: Curves.fastOutSlowIn)));
-    addAllListData();
 
+    // addAllListData();
+    FitBitToken test;
+    final User user = auth.currentUser;
+    final uid = user.uid;
+    final readFitbit = databaseReference.child('users/' + uid + "/fitbittoken/");
+    final read_connection = databaseReference.child('users/' + uid + "/fitbit_connection/");
+    readFitbit.once().then((DataSnapshot snapshot) {
+      read_connection.once().then((DataSnapshot connection) {
+        var temp = jsonDecode(jsonEncode(connection.value));
+        print("connection");
+        print(temp);
+        if(temp.toString().contains("false")){
+          addFitbit();
+          addAllListData();
+        }else{
+
+          if(snapshot.value != null){
+            test = FitBitToken.fromJson(jsonDecode(jsonEncode(snapshot.value)));
+            if(test != null){
+              fitbitToken = test.accessToken;
+              checkToken(test.accessToken);
+              addAllListData();
+              Future.delayed(const Duration(milliseconds: 2000), () {
+                setState(() {
+                  //isLoading = false;
+                });
+              });
+            }else{
+              addAllListData();
+            }
+          }else {
+            createClient().then((value) {
+
+              _client = value;
+              test =  FitBitToken.fromJson(jsonDecode(_client.credentials.toJson()));
+              final Fitbittokenref = databaseReference.child('users/' + uid + '/fitbittoken/');
+              Fitbittokenref.set({"accessToken": test.accessToken, "refreshToken": test.refreshToken, "idToken": test.idToken,
+                "tokenEndpoint": test.tokenEndpoint, "scopes": test.scopes, "expiration": test.expiration});
+              final readfitbitConnection = databaseReference.child('users/' + uid + '/fitbit_connection/');
+              readfitbitConnection.set({"isConnected": true});
+              if(test != null){
+                print("trap");
+                setState(() {
+                  fitbitToken = test.accessToken;
+                });
+                addAllListData();
+                Future.delayed(const Duration(milliseconds: 4000), (){
+                  setState(() {print("SETSTATE");
+                    // isLoading = false;
+                    fitbitToken = test.accessToken; });
+                });
+              }
+            });
+
+          }
+        }
+      });
+
+      print(snapshot.value);
+    });
     Future.delayed(const Duration(milliseconds: 2000),(){
       setState(() {
 
@@ -103,9 +180,27 @@ class _DashboardsState extends State<Dashboards>
     expandableState = List.generate(listViews.length, (index) => false);
     super.initState();
   }
+  void addFitbit() async {
 
+    const int count = 9;
+    listViews.add(
+      fitbit_connect(
+        animation: Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+            parent: widget.animationController,
+            curve:
+            Interval((1 / count) * 2, 1.0, curve: Curves.fastOutSlowIn))),
+        animationController: widget.animationController,
+      ),
+    );
+    Future.delayed(const Duration(milliseconds: 1000), (){
+      setState(() {
+        // isLoading = false;
+      });
+    });
+  }
   void addAllListData() {
     const int count = 5;
+    FitBitToken test;
 
     final User user = auth.currentUser;
     final uid = user.uid;
@@ -230,6 +325,41 @@ class _DashboardsState extends State<Dashboards>
       });
     });
   }
+  void checkToken(String accessToken) async{
+    print("CHECKING ACCESS TOKEN");
+    final User user = auth.currentUser;
+    final uid = user.uid;
+    var response = await http.get(Uri.parse("https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-03-27&sort=desc&offset=0&limit=1"),
+        headers: {
+          'Authorization': "Bearer " + accessToken
+        });
+    if(response.body.contains("Access token expired")){
+      print("Access token expired : Get Token now");
+      FitBitToken test;
+      createClient().then((value) {
+        _client = value;
+        test =  FitBitToken.fromJson(jsonDecode(_client.credentials.toJson()));
+        final Fitbittokenref = databaseReference.child('users/' + uid + '/fitbittoken/');
+        Fitbittokenref.set({"accessToken": test.accessToken, "refreshToken": test.refreshToken, "idToken": test.idToken,
+          "tokenEndpoint": test.tokenEndpoint, "scopes": test.scopes, "expiration": test.expiration});
+        final readfitbitConnection = databaseReference.child('users/' + uid + '/fitbit_connection/');
+        readfitbitConnection.set({"isConnected": true});
+        if(test != null){
+          fitbitToken = test.accessToken;
+          Future.delayed(const Duration(milliseconds: 1200), (){
+            setState(() {});
+          });
+        }
+      });
+    }else{
+      print("Token working");
+      fitbitToken = accessToken;
+      Future.delayed(const Duration(milliseconds: 1200), () {
+        setState(() {});
+      });
+    }
+  }
+
 
   Future<bool> getData() async {
     await Future<dynamic>.delayed(const Duration(milliseconds: 50));
@@ -257,7 +387,50 @@ class _DashboardsState extends State<Dashboards>
       ),
     );
   }
+  Future<oauth2.Client> createClient() async {
+    var exists = await credentialsFile.exists();
+    if (exists) {
+      print("CREDENTIALS");
+      var credentials =
+      oauth2.Credentials.fromJson(await credentialsFile.readAsString());
+      return oauth2.Client(credentials, identifier: identifier, secret: secret);
+    }
+    var grant = oauth2.AuthorizationCodeGrant(
+        identifier, authorizationEndpoint, tokenEndpoint,
+        secret: secret);
+    var authorizationUrl = grant.getAuthorizationUrl(redirectUrl,scopes: _scopes);
+    await redirect(authorizationUrl);
+    var responseUrl = await listen(redirectUrl);
+    String code = "";
+    if(responseUrl == null) {
+      throw Exception('response was null');
+    }else{
+      // print("NAG ELSE");
+      print("CODE = ");
+      code = responseUrl.toString();
+      code = code.replaceAll("localhost://callback?code=", "");
+      code = code.replaceAll("#_=_", "");
+      print(code);
+    }
+    var  readToken =await http.post(Uri.parse("https://api.fitbit.com/oauth2/token?code=$code&grant_type=authorization_code&redirect_uri=localhost://callback"),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': "Basic MjM4NFc0OjhmYTJkMzdiMGJkZjBiNzY2ZDZhMTRmOWNlNjQ2Mzhj"
+      },);
 
+    return await grant.handleAuthorizationResponse(responseUrl.queryParameters);
+  }
+  Future<void> redirect (Uri authurl) async{
+    if(await canLaunch(authurl.toString())){
+      await launch(authurl.toString());
+    }else{
+      throw Exception('Unable to launch $authurl');
+    }
+  }
+
+  Future<Uri> listen (Uri redirect) async {
+    return await getUriLinksStream().firstWhere((element) => element.toString().startsWith(redirect.toString()));
+  }
   @override
   Widget build(BuildContext context) {
     // Future.delayed(const Duration(milliseconds: 5000), () {
