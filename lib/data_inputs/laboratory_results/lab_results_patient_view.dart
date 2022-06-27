23aaa9f390a5705fd99a19b0cc6e18d7adf951fc
Enrickzz <enrico_cuison@dlsu.edu.ph>
2022-06-27 22:25:33 +0800

    lab res final reference for pop up

--- lib/data_inputs/laboratory_results/lab_results_patient_view.dart
+++ lib/data_inputs/laboratory_results/lab_results_patient_view.dart
@@ -101,10 +101,8 @@ class _lab_resultsState extends State<lab_results> {
                           setState(() {});
                         });
                       } else {
-                        ShowDialogRecomm(
-                                thisReturned.dialog.message,
-                                thisReturned.dialog.title,
-                                thisReturned.dialog.redirect)
+                        ShowDialogRecomm(thisReturned.dialog.message,
+                                thisReturned.dialog.title)
                             .then((value) {
                           print("AFTER DIALOG");
                           if (value1 != null) {
@@ -230,17 +228,16 @@ class _lab_resultsState extends State<lab_results> {
     );
   }
 
-  Future<void> ShowDialogRecomm(
-      String desc, String title, String redirect) async {
-    bool checktitle = true;
-    print("SHOWDIALOG PASOK");
-    if (title == "Peer Recommendation!") {
-      checktitle = false;
-      desc = desc + " Go to places tab to view new reviews!";
-    }
-    if (redirect == "None") {
-      checktitle = false;
-    }
+  Future<void> ShowDialogRecomm(String desc, String title) async {
+    // bool checktitle = true;
+    // print("SHOWDIALOG PASOK");
+    // if (title == "Peer Recommendation!") {
+    //   checktitle = false;
+    //   desc = desc + " Go to places tab to view new reviews!";
+    // }
+    // if (redirect == "None") {
+    //   checktitle = false;
+    // }
     return showDialog<void>(
       context: context,
       barrierDismissible: true, // user must tap button!
@@ -255,15 +252,15 @@ class _lab_resultsState extends State<lab_results> {
             ),
           ),
           actions: <Widget>[
-            Visibility(
-                visible: checktitle,
-                child: TextButton(
-                  child: Text('View'),
-                  onPressed: () {
-                    redirectDialog(redirect, title);
-                    Navigator.pop(context);
-                  },
-                )),
+            // Visibility(
+            //     visible: checktitle,
+            //     child: TextButton(
+            //       child: Text('View'),
+            //       onPressed: () {
+            //         redirectDialog(redirect, title);
+            //         Navigator.pop(context);
+            //       },
+            //     )),
             TextButton(
               child: Text('Got it!'),
               onPressed: () {
@@ -276,148 +273,148 @@ class _lab_resultsState extends State<lab_results> {
     );
   }
 
-  void redirectDialog(String redirect, String title) {
-    TickerProvider a;
-    AnimationController animationController = AnimationController(
-        duration: const Duration(milliseconds: 600), vsync: a);
-    if (redirect == "Food - Hemoglobin" && title == "Low Hemoglobin!") {
-      List<String> query = ["Spinach", "Legumes", "Brocolli"];
-      var rng = new Random();
-      fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => recommended_meals(
-                    mealsrecommendation: value,
-                    animationController: animationController)));
-      });
-    }
-    if (redirect == "Food - Cholesterol" && title == "High Cholesterol!") {
-      List<String> query = ["Tuna", "Walnuts", "Salmon"];
-      var rng = new Random();
-      fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => recommended_meals(
-                    mealsrecommendation: value,
-                    animationController: animationController)));
-      });
-    }
-    if (redirect == "Food - Cholesterol" && title == "High Cholesterol!") {
-      List<String> query = ["Tuna", "Walnuts", "Salmon"];
-      var rng = new Random();
-      fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => recommended_meals(
-                    mealsrecommendation: value,
-                    animationController: animationController)));
-      });
-    }
-    if (redirect == "Food - Potassium" && title == "Low Potassium!") {
-      List<String> query = ["Spinach", "Banana", "Beef"];
-      var rng = new Random();
-      fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => recommended_meals(
-                    mealsrecommendation: value,
-                    animationController: animationController)));
-      });
-    }
-    if (redirect == "Food - Glucose" && title == "Unusually Low Blood Sugar") {
-      List<String> query = ["Candy", "Banana", "Peanut Butter"];
-      var rng = new Random();
-      fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => recommended_meals(
-                    mealsrecommendation: value,
-                    animationController: animationController)));
-      });
-    }
-    if (redirect == "food_intake") {
-      if (title == "Meals too fatty!") {
-        List<String> query = ["Fish", "Lean Meat", "Vegetables"];
-        var rng = new Random();
-        fetchNutritionix(query[rng.nextInt(query.length)]);
-        fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => recommended_meals(
-                      mealsrecommendation: value,
-                      animationController: animationController)));
-        });
-      } else if (title == "Too much cholesterol!") {
-        List<String> query = ["Avocado", "Salmon", "Dark Chocolate"];
-        var rng = new Random();
-        fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => recommended_meals(
-                      mealsrecommendation: value,
-                      animationController: animationController)));
-        });
-      } else if (title == "Salty food!") {
-        List<String> query = ["Banana", "Tuna", "Salad"];
-        var rng = new Random();
-        fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => recommended_meals(
-                      mealsrecommendation: value,
-                      animationController: animationController)));
-        });
-      } else if (title == "Too much salt!") {
-        List<String> query = ["Banana", "Tuna", "Salad"];
-        var rng = new Random();
-        fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => recommended_meals(
-                      mealsrecommendation: value,
-                      animationController: animationController)));
-        });
-      } else if (title == "Had Coffee?") {
-        List<String> query = ["Green Tea", "Hot Tea", "Black Tea"];
-        var rng = new Random();
-        fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => recommended_meals(
-                      mealsrecommendation: value,
-                      animationController: animationController)));
-        });
-      } else if (title == "Too much Sugar!") {}
-    }
-    // if(recomm.title == "Peer Recommendation!"){
-    //   getPlace(recomm.redirect).then((value) {
-    //     Result2 val = value;
-    //     showModalBottomSheet(context: context,
-    //       isScrollControlled: true,
-    //       builder: (context) => SingleChildScrollView(
-    //         child: Container(
-    //         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-    //         child: info_place(this_info:  val, thisrating: checkrating2(val.placeId), type: type)
-    //         )
-    //       ),
-    //     );
-    //   });
-    //   // getReview(recomm.redirect);
-    //   Future.delayed(const Duration(milliseconds: 1200), (){
-    //     print("DELAYED");
-    //   });
-    // }
-  }
+  // void redirectDialog(String redirect, String title) {
+  //   TickerProvider a;
+  //   AnimationController animationController = AnimationController(
+  //       duration: const Duration(milliseconds: 600), vsync: a);
+  //   if (redirect == "Food - Hemoglobin" && title == "Low Hemoglobin!") {
+  //     List<String> query = ["Spinach", "Legumes", "Brocolli"];
+  //     var rng = new Random();
+  //     fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => recommended_meals(
+  //                   mealsrecommendation: value,
+  //                   animationController: animationController)));
+  //     });
+  //   }
+  //   if (redirect == "Food - Cholesterol" && title == "High Cholesterol!") {
+  //     List<String> query = ["Tuna", "Walnuts", "Salmon"];
+  //     var rng = new Random();
+  //     fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => recommended_meals(
+  //                   mealsrecommendation: value,
+  //                   animationController: animationController)));
+  //     });
+  //   }
+  //   if (redirect == "Food - Cholesterol" && title == "High Cholesterol!") {
+  //     List<String> query = ["Tuna", "Walnuts", "Salmon"];
+  //     var rng = new Random();
+  //     fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => recommended_meals(
+  //                   mealsrecommendation: value,
+  //                   animationController: animationController)));
+  //     });
+  //   }
+  //   if (redirect == "Food - Potassium" && title == "Low Potassium!") {
+  //     List<String> query = ["Spinach", "Banana", "Beef"];
+  //     var rng = new Random();
+  //     fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => recommended_meals(
+  //                   mealsrecommendation: value,
+  //                   animationController: animationController)));
+  //     });
+  //   }
+  //   if (redirect == "Food - Glucose" && title == "Unusually Low Blood Sugar") {
+  //     List<String> query = ["Candy", "Banana", "Peanut Butter"];
+  //     var rng = new Random();
+  //     fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => recommended_meals(
+  //                   mealsrecommendation: value,
+  //                   animationController: animationController)));
+  //     });
+  //   }
+  //   if (redirect == "food_intake") {
+  //     if (title == "Meals too fatty!") {
+  //       List<String> query = ["Fish", "Lean Meat", "Vegetables"];
+  //       var rng = new Random();
+  //       fetchNutritionix(query[rng.nextInt(query.length)]);
+  //       fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) => recommended_meals(
+  //                     mealsrecommendation: value,
+  //                     animationController: animationController)));
+  //       });
+  //     } else if (title == "Too much cholesterol!") {
+  //       List<String> query = ["Avocado", "Salmon", "Dark Chocolate"];
+  //       var rng = new Random();
+  //       fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) => recommended_meals(
+  //                     mealsrecommendation: value,
+  //                     animationController: animationController)));
+  //       });
+  //     } else if (title == "Salty food!") {
+  //       List<String> query = ["Banana", "Tuna", "Salad"];
+  //       var rng = new Random();
+  //       fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) => recommended_meals(
+  //                     mealsrecommendation: value,
+  //                     animationController: animationController)));
+  //       });
+  //     } else if (title == "Too much salt!") {
+  //       List<String> query = ["Banana", "Tuna", "Salad"];
+  //       var rng = new Random();
+  //       fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) => recommended_meals(
+  //                     mealsrecommendation: value,
+  //                     animationController: animationController)));
+  //       });
+  //     } else if (title == "Had Coffee?") {
+  //       List<String> query = ["Green Tea", "Hot Tea", "Black Tea"];
+  //       var rng = new Random();
+  //       fetchNutritionix(query[rng.nextInt(query.length)]).then((value) {
+  //         Navigator.push(
+  //             context,
+  //             MaterialPageRoute(
+  //                 builder: (context) => recommended_meals(
+  //                     mealsrecommendation: value,
+  //                     animationController: animationController)));
+  //       });
+  //     } else if (title == "Too much Sugar!") {}
+  //   }
+  //   // if(recomm.title == "Peer Recommendation!"){
+  //   //   getPlace(recomm.redirect).then((value) {
+  //   //     Result2 val = value;
+  //   //     showModalBottomSheet(context: context,
+  //   //       isScrollControlled: true,
+  //   //       builder: (context) => SingleChildScrollView(
+  //   //         child: Container(
+  //   //         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+  //   //         child: info_place(this_info:  val, thisrating: checkrating2(val.placeId), type: type)
+  //   //         )
+  //   //       ),
+  //   //     );
+  //   //   });
+  //   //   // getReview(recomm.redirect);
+  //   //   Future.delayed(const Duration(milliseconds: 1200), (){
+  //   //     print("DELAYED");
+  //   //   });
+  //   // }
+  // }
 
   Future<List<String>> _getDownloadLinks(List<Reference> refs) {
     return Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
